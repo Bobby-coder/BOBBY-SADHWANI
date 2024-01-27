6dff2a6517500ac68f405d9b3481fa8d6f292c39
Bobby-coder <bobbysadhwani612@gmail.com>
2024-01-27 15:09:45 +0530

    bio and dp changed

--- src/app/components/homepage/HomeHeroSection.js
+++ src/app/components/homepage/HomeHeroSection.js
@@ -6,13 +6,10 @@ const HomeHeroSection = () => {
     <>
       <div className="max-w-2xl">
         <h1 className="text-4xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Frontend Developer, Absolute JavaScrpt Enthusiast, and Tech Blogger.
+          Fullstack Developer, Absolute JavaScrpt Enthusiast, and Tech Blogger.
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          I’m Bobby an enthusiastic and passionate Frontend developer focused on
-          crafting clean and user friendly experiences. Good hands in responsive
-          Web Design and analyzing and fixing bugs quickly. Proficiency in code
-          optimization to improve the performance.
+        A tech-savvy and competent MERN developer with a passion for developing clean and user-friendly experiences, possessing hands-on expertise in responsive web design, bug analysis, rapid bug resolution, and code optimization techniques.
         </p>
         <div className="mt-6 flex gap-6">
           <Link
