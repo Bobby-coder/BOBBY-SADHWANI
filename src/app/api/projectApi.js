a7170f3c2e0432fa6724f1c2ef82211a758a7fb8
Bobby-coder <bobbysadhwani612@gmail.com>
2024-11-10 21:24:49 +0530

    Projects updated

--- src/app/api/projectApi.js
+++ src/app/api/projectApi.js
@@ -1,28 +1,19 @@
 const projectApi = [
   {
-    title: "Techie",
-    liveLink: "https://techie-blogs.vercel.app/",
-    githubLink: "https://github.com/Bobby-coder/Techie/",
+    title: "Primekart",
+    liveLink: "https://github.com/Bobby-coder/Primekart",
+    githubLink: "https://github.com/Bobby-coder/Primekart",
     imgSrc: `/images/notehack.webp`,
-    description: "Techie is a fullstack blogging website.",
+    description: "A full-stack e-commerce platform built with the MERN stack, featuring secure OTP-based authentication, dynamic product listings, and real-time shopping cart, save for later and wishlist management with drag-and-drop functionality. Integrated with Razorpay for smooth payments.",
   },
 
   {
-    title: "Social-boards",
-    liveLink: "https://social-boards.vercel.app/",
-    githubLink: "https://github.com/Bobby-coder/Social-boards",
+    title: "Kanban",
+    liveLink: "https://kanban-board-v9.vercel.app/",
+    githubLink: "https://github.com/Bobby-coder/Kanban",
     imgSrc: `/images/social-boards.png`,
     description:
-      "Social Boards is a fullstack feedback board website. User can build boards to organize ideas, share feedbacks, add images to showcase thoughts, join the conversation by replying to feedback, and upvote any feedback.",
-  },
-
-  {
-    title: "Awesome Captions",
-    liveLink: "https://awesome-captions.vercel.app/",
-    githubLink: "https://github.com/Bobby-coder/Awesome-Captions",
-    imgSrc: `/images/awesome-captions.png`,
-    description:
-      "Awesome Captions is a tool that allows users to upload a video, transcribe it, edit the timeframe and words, and add captions with desired colors.",
+      "A dynamic task management application featuring integrated CRUD functionality for seamless task operations. Users can effortlessly organize tasks with drag-and-drop capabilities, while local storage integration ensures data persistence across sessions, maintaining task integrity and accessibility.",
   },
 
   {
@@ -31,25 +22,16 @@ const projectApi = [
     githubLink: "https://github.com/Bobby-coder/Dad-Jokes",
     imgSrc: `/images/dadjokes.webp`,
     description:
-      "Dad Jokes is a fully functional react app which offers the various categories of jokes to enjoy.",
+      "Dad Jokes is a fully functional react app which offers the various categories of jokes to enjoy. 😁😂 I created this app for an open source project React Play.",
   },
 
   {
-    title: "CRYPTO SPY",
-    liveLink: "https://crypto-spy.netlify.app",
-    githubLink: "https://github.com/Bobby-coder/CRYPTO-SPY",
-    imgSrc: "/images/crypto-spy.webp",
+    title: "Weather-Forecast-Application",
+    liveLink: "https://weather-forecast-application-three.vercel.app/",
+    githubLink: "https://github.com/Bobby-coder/Weather-Forecast-Application",
+    imgSrc: `/images/awesome-captions.png`,
     description:
-      "Crypto Spy is a real time crypto tracker react app where a user can see the details of any cryptocurrency.",
-  },
-
-  {
-    title: "BOOKHUB",
-    liveLink: "https://ebook-hub.netlify.app",
-    githubLink: "https://github.com/Bobby-coder/BOOK-HUB",
-    imgSrc: `/images/bookhub.webp`,
-    description:
-      "BooKHub is a fully functional eBook finder react app where a user can see see the details of any eBook.",
+      "This application allows you to get current weather information and a 5-day forecast for any city. It also supports retrieving weather information based on your current location.",
   },
 ];
 
