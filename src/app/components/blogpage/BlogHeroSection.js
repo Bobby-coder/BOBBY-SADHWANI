0579a878d00682458bd77b23ee4204cfce2abb44
Bobby-coder <bobbysadhwani612@gmail.com>
2024-11-10 21:38:15 +0530

    Articles headline updated

--- src/app/components/blogpage/BlogHeroSection.js
+++ src/app/components/blogpage/BlogHeroSection.js
@@ -3,7 +3,7 @@ const BlogHeroSection = () => {
     <>
       <div className="max-w-2xl">
         <h1 className="text-4xl font-semibold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-          Writing on JavaScript, React JS, HTML & CSS.
+          Insights on JavaScript, React, Web Design & Frontend Development
         </h1>
         <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
           All of my blogs on JavaScript collected in chronological order.
